6858c0b66277560bdc47af7408b63ed1aa8bb3de
Pradeep Kumar <Pradeep.kumar@trivialworks.com>
2023-03-04 14:19:21 +0530

    update App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-// import { Counter } from './features/store/Counter';
 import './App.css';
 import Home from './features/cart/Home';
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
       <Home/>
     </div>
   );
